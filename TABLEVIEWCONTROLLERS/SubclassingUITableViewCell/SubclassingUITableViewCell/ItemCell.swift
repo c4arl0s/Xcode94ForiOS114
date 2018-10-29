4b1337e0a41ea1c8556049883b30a73a1105a5d0
carlos-santiago-2017 <c.santiago.cruz@gmail.com>
2018-10-28 21:35:30 -0600

    add my first commit of a new era

---
+++ TABLEVIEWCONTROLLERS/SubclassingUITableViewCell/SubclassingUITableViewCell/ItemCell.swift
@@ -0,0 +1,13 @@
+//
+//  ItemCell.swift
+//  SubclassingUITableViewCell
+//
+//  Created by Carlos Santiago Cruz on 17/10/18.
+//  Copyright © 2018 Carlos Santiago Cruz. All rights reserved.
+//
+
+import Foundation
+import UIKit
+class ItemCell: UITableViewCell {
+    
+}
