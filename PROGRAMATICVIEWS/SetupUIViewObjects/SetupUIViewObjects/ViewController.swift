e7dbd859422b0730f1e34576f16ab73791b29e6b
carlos-santiago-2017 <c.santiago.cruz@gmail.com>
2018-10-30 19:08:17 -0600

    I think, I forgot to drag-drop the CustomView Object, then instanciate customView.mainLabel.text but it does not work, keep trying

--- PROGRAMATICVIEWS/SetupUIViewObjects/SetupUIViewObjects/ViewController.swift
+++ PROGRAMATICVIEWS/SetupUIViewObjects/SetupUIViewObjects/ViewController.swift
@@ -9,11 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-  
+    @IBOutlet weak var customView: CustomView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        customView.mainLabel.text = "hello World!"
 
     }
 
