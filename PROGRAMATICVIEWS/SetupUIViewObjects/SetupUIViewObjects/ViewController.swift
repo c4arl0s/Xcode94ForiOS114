2c2ce7cdb39e55416069a3d7313c7c347979cf33
carlos-santiago-2017 <c.santiago.cruz@gmail.com>
2018-10-30 18:59:13 -0600

    SetupUIViewObjects code is not finished yet, I dont know why is crashing, you have to come back later and finish it

---
+++ PROGRAMATICVIEWS/SetupUIViewObjects/SetupUIViewObjects/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SetupUIViewObjects
+//
+//  Created by Carlos Santiago Cruz on 30/10/18.
+//  Copyright © 2018 Carlos Santiago Cruz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+
+    }
+
+
+}
+
